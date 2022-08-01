d2877b782be1e8ea13357613e472677a436982f8
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-01 14:58:50 -0700

    add buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import { renderGame } from './render-utils.js';
+import { renderGame, renderTeam } from './render-utils.js';
 const currentGameEl = document.getElementById('current-game-container');
 const pastGamesEl = document.getElementById('past-games-container');
 
@@ -11,61 +11,83 @@ const teamTwoSubtractButton = document.getElementById('team-two-subtract-button'
 const finishGameButton = document.getElementById('finish-game-button');
 const teamOneLabel = document.getElementById('team-one-name');
 const teamTwoLabel = document.getElementById('team-two-name');
+const teamOneInput = document.getElementById('team-one-input');
+const teamTwoInput = document.getElementById('team-two-input');
+const leftButtons = document.getElementById('left-buttons');
 
 // create an array to hold on to the state of past games
 
 let name1 = '';
-let name2 =  '';
+let name2 = '';
 let score1 = 0;
 let score2 = 0;
+let pastGames = [];
 
 nameFormButton.addEventListener('click', () => {
-    // get the name data from the form
 
-    // set the state to this data from the form
+    const teamOneName = teamOneInput.value;
+    const teamTwoName = teamTwoInput.value;
 
-    // reset the form values
+    teamOneLabel.textContent = teamOneName;
+    teamTwoLabel.textContent = teamTwoName;
 
-    // refresh the current game element with new data by calling the appropriate function
+    teamOneInput.value = '';
+    teamTwoInput.value = '';
+
+    renderGame(name1, name2, score1, score2);
+
 });
 
 
 teamOneAddButton.addEventListener('click', () => {
     // increment the current state for team one's score
-    
+    currentGameEl.textContent = '';
+    const team1Div = renderGame(name1, name2, score1, score2);
+    score1++;
     // refresh the current game element with new data by calling the appropriate function
+
+    currentGameEl.append(team1Div);
+
+    console.log(score1);
 });
 
 teamTwoAddButton.addEventListener('click', () => {
     // increment the current state for team two's score
+    currentGameEl.textContent = '';
+    const team2Div = renderGame(name1, name2, score1, score2);
+    score2++;
 
+    currentGameEl.append(team2Div);
     // refresh the current game element with new data by calling the appropriate function
+
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
     // decrement the current state for team one's score
-
+    score1--;
     // refresh the current game element with new data by calling the appropriate function
+
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
     // decrement the current state for team two's score
-
+    score2--;
     // refresh the current game element with new data by calling the appropriate function
+
 });
 
 finishGameButton.addEventListener('click', () => {
-    
+
     // add the current game to an array of games in state
     // it will be helpful to keep track of these games as objects with 4 properties, one for each piece of state we're tracking
     // for example, make an object like this: { name1: 'ducks', name2: 'bears' ,score1: 1, score2: 2 } 
     // then push it to your array in state
     // (be sure to make a new object. do not declare the object in global scope and mutate it for reuse. This would cause difficult bugs)
-    
+
     displayAllGames();
 
     // reset the state to zero and empty strings
-    
+
     // refresh the current game element with new data by calling the appropriate function
 });
 
@@ -79,7 +101,7 @@ function refreshCurrentGameEl() {
     // make a new gameEl here by calling renderGame with the approriate arguments. 
     // Check the renderGame function declaration in render-utils.js to figure out the correct arguments to pass to this function 
     // In render-utils.js as yourself: How many arguments does the function take? What order does it take them in?
-    
+
     gameEl.classList.add('current');
 
     currentGameEl.append(gameEl);
