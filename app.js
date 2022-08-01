21a3170d5b07827e3acd99e962df552fc6066aa3
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-01 16:49:17 -0700

    complete soccer scorekeeper app

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import functions and grab DOM elements
 import { renderGame } from './render-utils.js';
 const currentGameEl = document.getElementById('current-game-container');
 const pastGamesEl = document.getElementById('past-games-container');
@@ -13,8 +12,6 @@ const teamOneLabel = document.getElementById('team-one-name');
 const teamTwoLabel = document.getElementById('team-two-name');
 const teamOneInput = document.getElementById('team-one-input');
 const teamTwoInput = document.getElementById('team-two-input');
-
-// create an array to hold on to the state of past games
 
 let name1 = '';
 let name2 = '';
@@ -44,110 +41,87 @@ nameFormButton.addEventListener('click', () => {
 
 
 teamOneAddButton.addEventListener('click', () => {
-    // increment the current state for team one's score
+
     currentGameEl.textContent = '';
     score1++;
-    // refresh the current game element with new data by calling the appropriate function
-
 
     refreshCurrentGameEl();
 });
 
 teamTwoAddButton.addEventListener('click', () => {
-    // increment the current state for team two's score
+
     currentGameEl.textContent = '';
     score2++;
 
-    // refresh the current game element with new data by calling the appropriate function
     refreshCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
-    // decrement the current state for team one's score
+
     currentGameEl.textContent = '';
     score1--;
 
-    // refresh the current game element with new data by calling the appropriate function
     refreshCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
-    // decrement the current state for team two's score
+
     currentGameEl.textContent = '';
     score2--;
 
-    // refresh the current game element with new data by calling the appropriate function
     refreshCurrentGameEl();
+
 });
 
 finishGameButton.addEventListener('click', () => {
     
-    // add the current game to an array of games in state
-
-    
-    // it will be helpful to keep track of these games as objects with 4 properties, one for each piece of state we're tracking
-    // for example, make an object like this: { name1: 'ducks', name2: 'bears' ,score1: 1, score2: 2 }
     const currentGameData = {
         name1: `${name1}`,
         name2: `${name2}`,
         score1: `${score1}`,
         score2: `${score2}`
     };
-    // then push it to your array in state
-    pastGames.push(currentGameData);
-    // (be sure to make a new object. do not declare the object in global scope and mutate it for reuse. This would cause difficult bugs)
-    pastGamesEl.textContent = '';
 
-    for (let game of pastGames) {
-        const gameEl = renderGame(game.name1, game.name2, game.score1, game.score2);
-        pastGamesEl.append(gameEl);
-    }
+    pastGames.push(currentGameData);
+
+    pastGamesEl.textContent = '';
 
     displayAllGames();
 
-    // reset the state to zero and empty strings
     name1 = '';
     name2 = '';
     score1 = 0;
     score2 = 0;
+
     currentGameEl.textContent = '';
 
-    // refresh the current game element with new data by calling the appropriate function
     refreshCurrentGameEl();
+
 });
 
 function refreshCurrentGameEl() {
+
     currentGameEl.textContent = '';
 
     teamOneLabel.textContent = name1;
     teamTwoLabel.textContent = name2;
 
-    // const gameEl = . . . 
-    // make a new gameEl here by calling renderGame with the approriate arguments. 
-    // Check the renderGame function declaration in render-utils.js to figure out the correct arguments to pass to this function 
-    // In render-utils.js as yourself: How many arguments does the function take? What order does it take them in?
-
     const gameEl = renderGame(name1, name2, score1, score2);
-    
     
     gameEl.classList.add('current');
 
     currentGameEl.append(gameEl);
+
 }
 
 
 function displayAllGames() {
 
-    // clear out the past games list in the DOM
     pastGamesEl.textContent = '';
     
-    // loop through the past games in state
     for (let game of pastGames) {
         const gameEl = renderGame(game.name1, game.name2, game.score1, game.score2);
         pastGamesEl.append(gameEl);
     }
     
-    // use the renderGame function to render and append a past game for each past game in state
-    renderGame(name1, name2, score1, score2);
-    // again, review the renderGame function in render-utils.js. How many arguments does it take? What order does it take them in?
 }
